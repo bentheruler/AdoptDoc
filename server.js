003bea3c76861f4a1099bf79c86f25f3e0bec96c
Bencarson Agani <156955921+bentheruler@users.noreply.github.com>
2026-03-13 13:50:33 +0300

    fixed repeating Lines

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+
 
 const app = express();
 
-app.use(cors());
+const cors = require("cors");
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
@@ -14,6 +19,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log("Mongo Error:", err));
 
 app.use('/api/auth', require('./routes/auth'));
+
 
 app.get('/', (req, res) => {
   res.send("API Running...");
